a2222466f52e009c7c82546f560101b8d7ff2e76
arsenypoga <semiretoja@gmail.com>
2020-10-17 16:12:56 -0400

    moved path definitions into main

--- main.py
+++ main.py
@@ -6,11 +6,9 @@ import numpy as np
 
 from spam_filter import Bernoulli, Gauss, Multinomial
 
-test_file_path = Path("test-mails")
-train_file_path = Path("train-mails")
-
 
 def parse_message(text: str) -> List[str]:
+
     """Processes raw text message into a list of words without symbols
 
     Args:
@@ -25,9 +23,19 @@ def parse_message(text: str) -> List[str]:
     return word_list
 
 
-def generate_feature(path: Path, common_map: Set[str]):
+def generate_features(path: Path, common_words: List[str]) -> np.ndarray[np.float64]:
+    """Generates 2D array of where each row is an individual file,
+    and each column is score for the occurance of the specific word
+
+    Args:
+        path (Path): file path
+        common_words (Set[str]): List of common words 
+
+    Returns:
+        [type]: [description]
+    """
     files = list(path.iterdir())
-    features = np.zeros((len(files), len(common_map)))
+    features = np.zeros((len(files), len(common_words)))
     # file_index = 0
 
     for file in files:
@@ -36,7 +44,7 @@ def generate_feature(path: Path, common_map: Set[str]):
         file_counter = Counter(word_list)
 
         common_index = 0
-        for word in common_map:
+        for word in common_words:
 
             if word in file_counter:
                 # ipdb.set_trace()
@@ -47,6 +55,9 @@ def generate_feature(path: Path, common_map: Set[str]):
 
 
 def main():
+    test_file_path = Path("test-mails")
+    train_file_path = Path("train-mails")
+
     word_counter = Counter()
     files = list(train_file_path.iterdir())
 
@@ -58,23 +69,14 @@ def main():
     print("The maximum of most_common can be: ", len(word_counter))
     common_words = [k for k, _ in word_counter.most_common(3000)]
 
-    # construct model
-    # training feature matrix
-
-    # features = np.zeros((len(files), len(common_words)))
-
-    train_features = generate_feature(train_file_path, common_words)
+    train_features = generate_features(train_file_path, common_words)
 
     train_labels = np.zeros(len(files))
     train_labels[train_labels.size // 2 : train_labels.size] = 1.0
 
-    # verify model
-    # load test data
     files = list(test_file_path.iterdir())
-    # testing feature matrix
-    test_features = generate_feature(test_file_path, common_words)
+    test_features = generate_features(test_file_path, common_words)
 
-    # testing labels
     test_labels = np.zeros(len(files))
     test_labels[test_labels.size // 2 : test_labels.size] = 1.0
 
