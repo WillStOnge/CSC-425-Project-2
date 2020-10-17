8ba7f3a80b72c522fd625cbaeefeffc9a874aba9
arsenypoga <semiretoja@gmail.com>
2020-10-17 15:07:00 -0400

    First working complete refator of main

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+# import math
 import os
-import math
+from collections import Counter
+from pathlib import Path
+from typing import List, Set
+import ipdb
 import numpy as np
+
 from spam_filter import Bernoulli, Gauss, Multinomial
 
-test_file_path = "test-mails"
-train_file_path = "train-mails"
+test_file_path = Path("test-mails")
+train_file_path = Path("train-mails")
 
 wordMap = {}
 commonMap = []
@@ -12,165 +17,112 @@ commonMap = []
 most_common_word = 3000
 
 
-# avoid 0 terms in features
-smooth_alpha = 1.0
+def parse_message(text: str) -> List[str]:
+    """Processes raw text message into a list of words without symbols
 
-class_num = 2  # we have only two classes: ham and spam
-class_log_prior = [0.0, 0.0]  # probability for two classes
-feature_log_prob = np.zeros(
-    (class_num, most_common_word)
-)  # feature parameterized probability
-SPAM = 1  # spam class label
-HAM = 0  # ham class label
+    Args:
+        text (str): Message to be processed
 
-# read file anmes in the specific file path
-def read_file_names(file_path):
-    return os.listdir(file_path)
+    Returns:
+        List[str]: array of words
+    """
+    word_list = text.replace("\n", " ").split(" ")
+    word_list = [word for word in word_list if word.isalpha()]
 
-
-# read in the specific file
-def read_file(file):
-    content = ""
-    with open(file) as f:
-        for line in f:
-            line = line.strip()
-            if line:
-                content += line
-    return content
+    return word_list
 
 
-# count the total words
-def count_total_word(words):
-    for word in words:
-        if not word[0].isalpha():
-            continue
-        if not (word in wordMap.keys()):
-            wordMap[word] = 1
-        else:
-            count = wordMap[word]
-            wordMap[word] = count + 1
+def generate_feature(path: Path, common_map: Set[str]):
+    files = list(path.iterdir())
+    features = np.zeros((len(files), len(common_map)))
+    # file_index = 0
 
-
-# count the word in one file
-def count_word(words, singleWordMap={}):
-    for word in words:
-        if not word[0].isalpha():
-            continue
-        if not (word in singleWordMap.keys()):
-            singleWordMap[word] = 1
-        else:
-            count = singleWordMap[word]
-            singleWordMap[word] = count + 1
-
-
-# find the most common words in files store in commonMap
-def most_common():
-    # sort the wordMap
-    sort_wordMap = {
-        k: v for k, v in sorted(wordMap.items(), key=lambda x: x[1], reverse=True)
-    }
-
-    # add the most common words into commonMap
-    index = 0
-    for key in sort_wordMap.keys():
-        if index < most_common_word:
-            commonMap.append(key)
-        else:
-            break
-        index += 1
-
-
-# generate features according to commonMap
-def generate_feature(features, path, files):
-    singleWordMap = {}
-    file_index = 0
     for file in files:
-        singleWordMap = {}
-        content = read_file(path + "/" + file)
-        content.replace("\n", "")
-        contents = content.split(" ")
-        # print(contents)
-        count_word(contents, singleWordMap)
+        text = file.read_text()
+        word_list = parse_message(text)
+        file_counter = Counter(word_list)
 
-        for key1 in singleWordMap.keys():
-            common_index = 0
-            for key2 in commonMap:
-                if key1 == key2:
-                    features[file_index][common_index] = singleWordMap[key1]
-                common_index += 1
-        file_index += 1
+        common_index = 0
+        for word in common_map:
 
-
-# construct dictionary
-files = read_file_names(train_file_path)
-
-for i in range(len(files)):
-    content = read_file(train_file_path + "/" + files[i])
-    content.replace("\n", "")
-    contents = content.split(" ")
-    count_total_word(contents)
-
-print("The maximum of most_common can be: ", len(wordMap))
-
-most_common()
+            if word in file_counter:
+                # ipdb.set_trace()
+                features[files.index(file)][common_index] = file_counter[word]
+            common_index += 1
+        # file_index += 1
+    return features
 
 
-# construct model
-# training feature matrix
-train_features = np.zeros((len(files), len(commonMap)))
-generate_feature(train_features, train_file_path, files)
+def main():
 
-# training labels
-train_labels = np.zeros(len(files))
-for i in range(len(files) // 2, len(files)):
-    train_labels[i] = 1
+    # construct dictionary
+    word_counter = Counter()
+    for file in train_file_path.iterdir():
+        text = file.read_text()
+        word_list = parse_message(text)
+        word_counter += Counter(word_list)
 
-# verify model
-# load test data
-files = read_file_names(test_file_path)
-# testing feature matrix
-test_features = np.zeros((len(files), len(commonMap)))
-generate_feature(test_features, test_file_path, files)
+    print("The maximum of most_common can be: ", len(word_counter))
+    common_words = [k for k, v in word_counter.most_common(3000)]
 
-# testing labels
-test_labels = np.zeros(len(files))
-for i in range(len(files) // 2, len(files)):
-    test_labels[i] = 1
+    # construct model
+    # training feature matrix
+
+    files = list(train_file_path.iterdir())
+    # features = np.zeros((len(files), len(common_words)))
+
+    train_features = generate_feature(train_file_path, common_words)
+
+    train_labels = np.zeros(len(files))
+    train_labels[train_labels.size // 2 : train_labels.size] = 1.0
+
+    # verify model
+    # load test data
+    files = list(test_file_path.iterdir())
+    # testing feature matrix
+    test_features = generate_feature(test_file_path, common_words)
+
+    # testing labels
+    test_labels = np.zeros(len(files))
+    test_labels[test_labels.size // 2 : test_labels.size] = 1.0
+
+    # Multinomial Naive Bayes start
+    # print(train_labels)
+    # train model
+    multinomial = Multinomial()
+    multinomial.MultinomialNB(train_features, train_labels)
+    # test model
+    classes = multinomial.MultinomialNB_predict(test_features)
+
+    error = 0
+    for i in range(len(files)):
+        if test_labels[i] == classes[i]:
+            error += 1
+    print("Multinomial Naive Bayes: ", float(error) / float(len(test_labels)))
+    # Multinomial Naive Bayes end
+
+    # Bernoulli Naive Bayes start
+    BernoulliNB = Bernoulli()
+    BernoulliNB.BernoulliNB(train_features, train_labels)
+    classes = BernoulliNB.BernoulliNB_predict(test_features)
+    error = 0
+    for i in range(len(files)):
+        if test_labels[i] == classes[i]:
+            error += 1
+    print("Bernoulli Naive Bayes: ", float(error) / float(len(test_labels)))
+    # Bernoulli Naive Bayes end
+
+    # Gaussian Naive Bayes start
+    GaussianNB = Gauss()
+    GaussianNB.GaussianNB(train_features, train_labels)
+    classes = GaussianNB.GaussianNB_predict(test_features)
+    error = 0
+    for i in range(len(files)):
+        if test_labels[i] == classes[i]:
+            error += 1
+    print("Gaussian Naive Bayes: ", float(error) / float(len(test_labels)))
+    # Gaussian Naive Bayes end
 
 
-# Multinomial Naive Bayes start
-# print(train_labels)
-# train model
-MultinomialNB = Multinomial()
-MultinomialNB.MultinomialNB(train_features, train_labels)
-# test model
-classes = MultinomialNB.MultinomialNB_predict(test_features)
-error = 0
-for i in range(len(files)):
-    if test_labels[i] == classes[i]:
-        error += 1
-print("Multinomial Naive Bayes: ", float(error) / float(len(test_labels)))
-# Multinomial Naive Bayes end
-
-
-# Bernoulli Naive Bayes start
-BernoulliNB = Bernoulli()
-BernoulliNB.BernoulliNB(train_features, train_labels)
-classes = BernoulliNB.BernoulliNB_predict(test_features)
-error = 0
-for i in range(len(files)):
-    if test_labels[i] == classes[i]:
-        error += 1
-print("Bernoulli Naive Bayes: ", float(error) / float(len(test_labels)))
-# Bernoulli Naive Bayes end
-
-# Gaussian Naive Bayes start
-GaussianNB = Gauss()
-GaussianNB.GaussianNB(train_features, train_labels)
-classes = GaussianNB.GaussianNB_predict(test_features)
-error = 0
-for i in range(len(files)):
-    if test_labels[i] == classes[i]:
-        error += 1
-print("Gaussian Naive Bayes: ", float(error) / float(len(test_labels)))
-# Gaussian Naive Bayes end
+if __name__ == "__main__":
+    main()
