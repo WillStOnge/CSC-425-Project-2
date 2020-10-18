150b9b649149525691b9e8d8fb00cd3039d8c9ce
Trey Regruth <Treyregruth@gmail.com>
2020-10-18 15:27:04 -0400

    Revert "Wrote the code for mean and standard deviation"
    This reverts commit f13e6cb6a5a06fa8c5207bfda5ea9038eea6e71e.

--- spam_filter/gauss.py
+++ spam_filter/gauss.py
@@ -26,10 +26,8 @@ class Gauss:
             sum_ham = 0
             sum_spam = 0
             for j in range(len(features)):
-                if labels[j] == 0:
-                    sum_ham += features[j][i]
-                else:
-                    sum_spam += features[j][i]
+                sum_ham += features[j][i]
+                sum_spam += features[j][i]
             mean[0][i] = sum_ham / (len(labels) - np.count_nonzero(labels))
             mean[1][i] = sum_spam / np.count_nonzero(labels)
         # calculate the standard deviations
@@ -37,13 +35,10 @@ class Gauss:
             seq_ham = 0
             seq_spam = 0
             for y in range(len(features)):
-                if labels[y] == 0:
-                    seq_ham += math.pow((features[y][x] - mean[0][x]), 2)
-                else:
-                    seq_spam += math.pow((features[y][x] - mean[1][x]), 2)
+                seq_ham += math.pow((features[y][x] - mean[0][x]), 2)
+                seq_spam += math.pow((features[y][x] - mean[1][x]), 2)
             std[0][x] = math.sqrt(seq_ham / (len(labels) - np.count_nonzero(labels)))
-            std[1][x] = math.sqrt(seq_spam / np.count_nonzero(labels))
-            print("std", std[0][x], "mean", mean[0][x], "\nstd", std[1][x], "mean", mean[1][x])
+            std[1][x] = math.sqrt(seq_ham / np.count_nonzero(labels))
 
     # Gaussian Naive Bayes prediction
     def GaussianNB_predict(self, features):
