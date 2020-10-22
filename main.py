d6cde3e87ad73db5979e9a9d3b798637dc2ca03d
arsenypoga <semiretoja@gmail.com>
2020-10-22 17:12:36 -0400

    Converted the scikit based multinomial test into unittest hard coded one

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def main():
 
     common_map = [w for w, _ in word_counter.most_common(3000)]
 
-    train_features = generate_feature(common_map, train_file_path)
+    train_features = generate_features(common_map, train_file_path)
 
     # training labels
     train_labels = np.zeros(len(files))
@@ -41,7 +41,7 @@ def main():
 
     files = list(test_file_path.iterdir())
     # testing feature matrix
-    test_features = generate_feature(common_map, test_file_path)
+    test_features = generate_features(common_map, test_file_path)
 
     # testing labels
     test_labels = np.zeros(len(files))
@@ -88,100 +88,12 @@ def parse_message(text: str) -> List[str]:
         List[str]: array of words
     """
     word_list = text.replace("\n", " ").split(" ")
-<<<<<<< HEAD
-    word_list = [word for word in word_list if word.isalpha()]
-=======
     word_list = [word for word in word_list if word != "" and word[0].isalpha()]
->>>>>>> master
 
     return word_list
 
 
-<<<<<<< HEAD
-def generate_features(path: Path, common_words: Set[str]):
-    """Generates 2D array of where each row is an individual file,
-    and each column is score for the occurance of the specific word
-
-    Args:
-        path (Path): file path
-        common_words (Set[str]): List of common words 
-
-    Returns:
-        [type]: [description]
-    """
-    files = list(path.iterdir())
-    features = np.zeros((len(files), len(common_words)))
-    # file_index = 0
-
-    for file in files:
-        text = file.read_text()
-        word_list = parse_message(text)
-        file_counter = Counter(word_list)
-
-        common_index = 0
-        for word in file_counter:
-            if word in common_words:
-                features[files.index(file)][common_index] = file_counter[word]
-
-            common_index += 1
-
-    return features
-
-
-def main():
-    test_file_path = Path("test-mails")
-    train_file_path = Path("train-mails")
-
-    word_counter = Counter()
-    files = list(train_file_path.iterdir())
-
-    for file in files:
-        text = file.read_text()
-        word_list = parse_message(text)
-        word_counter += Counter(word_list)
-
-    print("The maximum of most_common can be: ", len(word_counter))
-    common_words = {k for k, _ in word_counter.most_common(3000)}
-
-    train_features = generate_features(train_file_path, common_words)
-    train_labels = np.zeros(len(files))
-
-    train_labels[train_labels.size // 2 : train_labels.size] = 1.0
-
-    files = list(test_file_path.iterdir())
-    test_features = generate_features(test_file_path, common_words)
-
-    test_labels = np.zeros(len(files))
-    test_labels[test_labels.size // 2 : test_labels.size] = 1.0
-
-    # Multinomial Naive Bayes start
-    multinomial = Multinomial()
-    multinomial.MultinomialNB(train_features, train_labels)
-    classes = multinomial.MultinomialNB_predict(test_features)
-
-    error = (test_labels == classes).sum()
-
-    print("Multinomial Naive Bayes: ", float(error) / float(len(test_labels)))
-
-    # # Bernoulli Naive Bayes start
-    BernoulliNB = Bernoulli()
-    BernoulliNB.BernoulliNB(train_features, train_labels)
-    classes = BernoulliNB.BernoulliNB_predict(test_features)
-
-    error = (test_labels == classes).sum()
-
-    print("Bernoulli Naive Bayes: ", float(error) / float(len(test_labels)))
-
-    # Gaussian Naive Bayes start
-    GaussianNB = Gauss()
-    GaussianNB.GaussianNB(train_features, train_labels)
-    classes = GaussianNB.GaussianNB_predict(test_features)
-
-    error = (test_labels == classes).sum()
-
-    print("Gaussian Naive Bayes: ", float(error) / float(len(test_labels)))
-=======
-def generate_feature(common_map: List[str], path: Path) -> List[List[str]]:
+def generate_features(common_map: List[str], path: Path) -> List[List[str]]:
     """Generates a 2 dimensional feature array of shape (file, common_word)
 
     Args:
@@ -209,7 +121,6 @@ def generate_feature(common_map: List[str], path: Path) -> List[List[str]]:
             common_index += 1
         file_index += 1
     return features
->>>>>>> master
 
 
 if __name__ == "__main__":
--- spam_filter/multinomial.py
+++ spam_filter/multinomial.py
@@ -1,69 +1,89 @@
 import math, enum, numpy as np
 
+
 class Classification(enum.Enum):
-	HAM = 0
-	SPAM = 1
+    HAM = 0
+    SPAM = 1
+
 
 NUM_CLASSES = len(Classification)
 MOST_COMMON_WORD = 3000
 SMOOTH_ALPHA = 1.0
 
+
 class MultinomialNaiveBayes:
-	""" Multinomial Naive Bayes """
-	class_log_prior: np.ndarray
-	feature_log_prob: np.ndarray
+    """ Multinomial Naive Bayes """
 
-	def __init__(self, features: np.array, labels: np.array):
-		""" Trains the model using Multinomial NB. """
-		self.class_log_prior = np.zeros(NUM_CLASSES, dtype=np.float64)
-		self.feature_log_prob = np.zeros((NUM_CLASSES, MOST_COMMON_WORD))
+    class_log_prior: np.ndarray
+    feature_log_prob: np.ndarray
 
-		# Get distribution of ham vs spam in training set.
-		self.class_log_prior[Classification.HAM.value] = math.log(np.sum(labels == Classification.HAM.value))
-		self.class_log_prior[Classification.SPAM.value] = math.log(np.sum(labels == Classification.SPAM.value))
-		
-		# Numerator of conditional probabilty
-		ham_words = np.tile(SMOOTH_ALPHA, MOST_COMMON_WORD)
-		spam_words = np.tile(SMOOTH_ALPHA, MOST_COMMON_WORD)
+    def __init__(self, features: np.array, labels: np.array):
+        """ Trains the model using Multinomial NB. """
+        self.class_log_prior = np.zeros(NUM_CLASSES, dtype=np.float64)
+        self.feature_log_prob = np.zeros((NUM_CLASSES, MOST_COMMON_WORD))
 
-		# Denominator of conditional probabilty
-		ham_sum = MOST_COMMON_WORD * SMOOTH_ALPHA
-		spam_sum = MOST_COMMON_WORD * SMOOTH_ALPHA
+        # Get distribution of ham vs spam in training set.
+        self.class_log_prior[Classification.HAM.value] = math.log(
+            np.sum(labels == Classification.HAM.value)
+        )
+        self.class_log_prior[Classification.SPAM.value] = math.log(
+            np.sum(labels == Classification.SPAM.value)
+        )
 
-		for i in range(features.shape[0]):
-			for j in range(features.shape[1]):
-				if labels[i] == Classification.HAM.value:
-					ham_words[j] += features[i][j]
-					ham_sum += features[i][j]
-				else:
-					spam_words[j] += features[i][j]
-					spam_sum += features[i][j]
+        # Numerator of conditional probabilty
+        ham_words = np.tile(SMOOTH_ALPHA, MOST_COMMON_WORD)
+        spam_words = np.tile(SMOOTH_ALPHA, MOST_COMMON_WORD)
 
-		for i in range(features.shape[1]):
-			self.feature_log_prob[Classification.HAM.value][i] = math.log(ham_words[i] / ham_sum)
-			self.feature_log_prob[Classification.SPAM.value][i] = math.log(spam_words[i] / spam_sum)
-	
-	def predict(self, features: np.array):
-		""" Classify an array of emails feature vectors. """
-		classes = np.zeros(features.shape[0])
+        # Denominator of conditional probabilty
+        ham_sum = MOST_COMMON_WORD * SMOOTH_ALPHA
+        spam_sum = MOST_COMMON_WORD * SMOOTH_ALPHA
 
-		word_occurences = np.sum(features, axis=0)
+        for i in range(features.shape[0]):
+            for j in range(features.shape[1]):
+                if labels[i] == Classification.HAM.value:
+                    ham_words[j] += features[i][j]
+                    ham_sum += features[i][j]
+                else:
+                    spam_words[j] += features[i][j]
+                    spam_sum += features[i][j]
 
-		# Loop through each email
-		for i in range(features.shape[0]):
-			ham_prob = 0
-			spam_prob = 0
+        for i in range(features.shape[1]):
+            self.feature_log_prob[Classification.HAM.value][i] = math.log(
+                ham_words[i] / ham_sum
+            )
+            self.feature_log_prob[Classification.SPAM.value][i] = math.log(
+                spam_words[i] / spam_sum
+            )
 
-			# Calculate the probability that the email is spam or ham.
-			for j in range(features.shape[1]):
-				ham_prob += (self.feature_log_prob[Classification.HAM.value][j] * features[i][j])
-				spam_prob += (self.feature_log_prob[Classification.SPAM.value][j] * features[i][j])
+    def predict(self, features: np.array):
+        """ Classify an array of emails feature vectors. """
+        classes = np.zeros(features.shape[0])
 
-			# Add class_log_prior value
-			ham_prob += self.class_log_prior[Classification.HAM.value]
-			spam_prob += self.class_log_prior[Classification.SPAM.value]
+        word_occurences = np.sum(features, axis=0)
 
-			# Determine which probability is higher.
-			classes[i] = Classification.HAM.value if ham_prob > spam_prob else Classification.SPAM.value
-		
-		return classes
+        # Loop through each email
+        for i in range(features.shape[0]):
+            ham_prob = 0
+            spam_prob = 0
+            # Calculate the probability that the email is spam or ham.
+            for j in range(features.shape[1]):
+                ham_prob += (
+                    self.feature_log_prob[Classification.HAM.value][j] * features[i][j]
+                )
+                spam_prob += (
+                    self.feature_log_prob[Classification.SPAM.value][j] * features[i][j]
+                )
+
+            # Add class_log_prior value
+            ham_prob += self.class_log_prior[Classification.HAM.value]
+            spam_prob += self.class_log_prior[Classification.SPAM.value]
+
+            # Determine which probability is higher.
+            classes[i] = (
+                Classification.HAM.value
+                if ham_prob > spam_prob
+                else Classification.SPAM.value
+            )
+
+        return classes
+
