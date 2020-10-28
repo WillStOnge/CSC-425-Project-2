43513bca95716e832d5e4f8c75b0233e7b57e193
Will St. Onge <ongewill@gmail.com>
2020-10-27 21:16:34 -0400

    Updated analysis script

--- analysis.py
+++ analysis.py
@@ -3,7 +3,7 @@ from collections import Counter
 from pathlib import Path
 from typing import List
 from spam_filter import BernoulliNB, GaussianNB, MultinomialNB
-from sklearn.naive_bayes import BernoulliNB, GaussianNB, MultinomialNB
+from sklearn.naive_bayes import BernoulliNB as BernoulliNB_sklearn, GaussianNB as GaussianNB_sklearn, MultinomialNB as MultinomialNB_sklearn
 from main import parse_message, generate_features
 
 
@@ -19,11 +19,11 @@ def main():
         word_counter += Counter(word_list)
 
     common_map = [w for w, _ in word_counter.most_common(3000)]
-    train_features = generate_feature(common_map, train_file_path)
+    train_features = generate_features(common_map, train_file_path)
     train_labels = np.zeros(len(files))
     train_labels[train_labels.size // 2:train_labels.size] = 1.0
     files = list(test_file_path.iterdir())
-    test_features = generate_feature(common_map, test_file_path)
+    test_features = generate_features(common_map, test_file_path)
     test_labels = np.zeros(len(files))
     test_labels[test_labels.size // 2:test_labels.size] = 1.0
 
@@ -33,45 +33,45 @@ def main():
     multinomial = MultinomialNB(train_features, train_labels)
     multinomial.predict(test_features)
 
-    print("Multinomial execution time: {:.2}".format(time.time() - start_time))
+    print("Multinomial execution time: {0:.2f}".format(time.time() - start_time))
     start_time = time.time()
 
     # Bernoulli Naive Bayes
     bernoulli = BernoulliNB(train_features, train_labels)
     bernoulli.predict(test_features)
 
-    print("Bernoulli execution time: {:.2}".format(time.time() - start_time))
+    print("Bernoulli execution time: {0:.2f}".format(time.time() - start_time))
     start_time = time.time()
 
     # Gaussian Naive Bayes
     gaussian = GaussianNB(train_features, train_labels)
     gaussian.predict(test_features)
 
-    print("Gaussian execution time: {:.2}".format(time.time() - start_time))
+    print("Gaussian execution time: {0:.2f}".format(time.time() - start_time))
     start_time = time.time()
 
     # SKLearn Multinomial Naive Bayes
-    multinomial_sklearn = MultinomialNB()
+    multinomial_sklearn = MultinomialNB_sklearn()
     multinomial_sklearn.fit(train_features, train_labels)
     classes_sklearn = multinomial_sklearn.predict(test_features)
 
-    print("SKLearn Multinomial execution time: {:.2}".format(time.time() - start_time))
+    print("SKLearn Multinomial execution time: {0:.2f}".format(time.time() - start_time))
     start_time = time.time()
 
     # SKLearn Bernoulli Naive Bayes
-    bernoulli_sklearn = BernoulliNB()
+    bernoulli_sklearn = BernoulliNB_sklearn()
     bernoulli_sklearn.fit(train_features, train_labels)
     bernoulli_sklearn.predict(test_features)
 
-    print("SKLearn Bernoulli execution time: {:.2}".format(time.time() - start_time))
+    print("SKLearn Bernoulli execution time: {0:.2f}".format(time.time() - start_time))
     start_time = time.time()
 
     # SKLearn Gaussian Naive Bayes
-    gaussian_sklearn = GaussianNB()
+    gaussian_sklearn = GaussianNB_sklearn()
     gaussian_sklearn.fit(train_features, train_labels)
     gaussian_sklearn.predict(test_features)
 
-    print("SKLearn Gaussian execution time: {:.2}".format(time.time() - start_time))
+    print("SKLearn Gaussian execution time: {0:.2f}".format(time.time() - start_time))
 
 
 if __name__ == "__main__":
